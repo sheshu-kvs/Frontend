3fc2589c6f1df79f2f0fe147b5c180fad2f3c028
sheshu-kvs <kvs24161@gmail.com>
2025-09-19 20:42:42 +0530

    DOM Example

---
+++ JavaScript/script.js
@@ -0,0 +1,45 @@
+// // let id=document.getElementById("id")
+// let id=document.getElementById("id").innerHTML="What Color";
+
+// console.log(id);
+
+
+
+// console.dir() it will return the properties and the methods of the object..
+
+// console.dir(document.body.div);
+
+
+// // to access the html content with the id we can getElementById()
+
+ let id=document.getElementById("id").innerHTML="This is the id select from the Html"
+//  console.dir(id)0000000000000
+//  console.log(id)000000000000
+let class12=docu0000000000000000000000000000000000000000000000000000000000000ment.getElementsByClassName("p-class");
+
+// getElementsByClassName : it will return the collection of the elements like the array
+// to display that values we can iterate 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+// for(let val of class12){
+//     val=val.innerHTML="getElementsByClassName return the collection of the elements";
+//     console.log(val);
+// }
+
+
+
+let singleclass=document.getElementsByTagName("p");
+
+for(let val of singleclass ){
+    val=val.innerHTML="Hello of multiple class with iteration";
+    console.log(val);
+}
+// console.log(singleclass);
+
+
+
+
+
+
+ 
+
+let 
