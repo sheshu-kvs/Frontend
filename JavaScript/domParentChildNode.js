cf184d7ef6dae7f756402cf1415d025fc129cae3
sheshu-kvs <kvs24161@gmail.com>
2025-09-22 11:04:34 +0530

    Parent, Child, Sibling, Node differences

---
+++ JavaScript/domParentChildNode.js
@@ -0,0 +1,17 @@
+// Each element = a node is the tree
+
+// parent ,child ,sibling
+
+// parent is the div element
+// div is the parent and  child elements are the h2 and the p element
+
+// h2 and the p are the siblings
+
+// think in the dom is everything is the node
+
+// like three 
+// element nodes <div>,<h2>,<p>
+// text nodes, heading and the paragraph
+//  attribute nodes like class="child1", class="child2"
+
+
