da531e101bc5019da400d1b74acd97e299eda1b2
sheshu-kvs <kvs24161@gmail.com>
2025-09-22 11:05:47 +0530

    Attibutes in the Dom , and also Seeting the Attribute,Getting the attribute

---
+++ JavaScript/domsetAttibutejs.js
@@ -0,0 +1,44 @@
+
+// the type,id,placeholder this are the attributes
+// in the beloiw example we are getting the attributes 
+// let un=document.querySelector("#username");
+// console.log(un.getAttribute("type"));
+
+
+// we can also the setattribute  in this will use two parameters like the
+// attibute and the value
+// using setAttribut("attribute","value")
+
+// let un=document.querySelector("#username");
+
+// console.log(un.setAttribute("placeholder","Sir Please Enter ur Name !"));
+
+
+// --hasAttribute we will use to declare the whether the attribute is there are not???'
+// if it is means it will say true.
+
+// let un=document.querySelector("#username");
+// console.log(un.hasAttribute("placeholder"));
+
+// removeAttribute()
+// remove the attribute are used to the  
+
+
+// let un=document.querySelector("#username");
+// console.log(un.removeAttribute("id"));
+
+
+// accesing the attributes directly??
+
+// let val=document.querySelector("#username");
+// console.log(val.id);
+
+
+
+// dom css
+// use this syntax
+
+let val=document.querySelector(".phone");
+// console.log(val);
+val.style.fontfamily="450px";
+
