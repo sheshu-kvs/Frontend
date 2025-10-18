71fcf96d9bfe6500b8fa92f4384ce8c1b23e3f5c
sheshu-kvs <kvs24161@gmail.com>
2025-10-18 23:40:17 +0530

    Fetch Concepts

--- HTML JS TASK/fetch.js
+++ HTML JS TASK/fetch.js
@@ -11,9 +11,30 @@ const data = async () => {
 //   para.innerHTML = `
 // <p><b>Setup</b>${anotheresponse.setup}</p>`;
 
+
 para.innerText=anotheresponse.setup;
+
 // console.log(anotheresponse.setup);
 };
 // data();
 
 btn.addEventListener("click", data);
+
+
+
+
+// function result(){
+//   return new Promise((resolve,reject)=>{
+//     // console.log(url);
+//     resolve("A Data Was Found ");
+//   })
+// }
+
+
+// result()
+// .then((res)=>{
+//   res.fetch(url);
+// })
+// .then((res)=>{
+//   res.json();
+// })
